88d63b0f9e02cbe6039b3857bbc269a996d71cd7
Minh <vanminh00052@gmail.com>
2026-04-18 16:30:46 +0700

    Add solution for Lucian's Luscious Lasagna

---
+++ lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,15 @@
+class Lasagna
+{
+    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int Lasagna.ExpectedMinutesInOven()
+    {
+        var Lng = new Lasagna();
+        
+    }
+
+    // TODO: define the 'RemainingMinutesInOven()' method
+
+    // TODO: define the 'PreparationTimeInMinutes()' method
+
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+}
