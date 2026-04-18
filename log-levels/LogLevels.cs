cae2735f6a3279561c39845530911f1c47d6b8ea
Minh <vanminh00052@gmail.com>
2026-04-18 23:07:55 +0700

    Add solution for Log Level

---
+++ log-levels/LogLevels.cs
@@ -0,0 +1,17 @@
+static class LogLine
+{
+    public static string Message(string logLine)
+    {
+        
+    }
+
+    public static string LogLevel(string logLine)
+    {
+    }
+
+    public static string Reformat(string logLine)
+    {
+
+    }
+    
+}
