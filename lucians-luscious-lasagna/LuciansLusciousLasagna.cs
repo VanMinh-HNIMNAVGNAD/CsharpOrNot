cbfc1d6470540bd3e0ac59f45c93e9f506c84e1a
Minh <vanminh00052@gmail.com>
2026-04-18 17:30:37 +0700

    Add solution for Lucian's Luscious Lasagna

--- lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,15 +1,40 @@
+using Microsoft.VisualBasic;
+
 class Lasagna
 {
     // TODO: define the 'ExpectedMinutesInOven()' method
-    public int Lasagna.ExpectedMinutesInOven()
+    public int ExpectedMinutesInOven()
     {
-        var Lng = new Lasagna();
-        
+        return 40;
     }
 
     // TODO: define the 'RemainingMinutesInOven()' method
-
+    public int RemainingMinutesInOven(int remaining)
+    {
+        return ExpectedMinutesInOven() - remaining;
+    }
     // TODO: define the 'PreparationTimeInMinutes()' method
 
+    public int PreparationTimeInMinutes(int numberoflayer)
+    {
+        return numberoflayer * 2;        
+    }
+
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int numberoflayer, int remaining)
+    {
+        return PreparationTimeInMinutes(numberoflayer) + remaining;
+    }
+
+    static void Main()
+    {
+        var Lasagna = new Lasagna();
+        int timetoremaining = Lasagna.RemainingMinutesInOven(30);
+        int timetoprepare = Lasagna.PreparationTimeInMinutes(2);
+        int total = Lasagna.ElapsedTimeInMinutes(3, 20);
+
+        Console.WriteLine(timetoremaining);
+        Console.WriteLine(timetoprepare);
+        Console.WriteLine(total);
+    }
 }
