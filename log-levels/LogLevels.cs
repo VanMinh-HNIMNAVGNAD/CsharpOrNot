9ebd08bf6ba108a2a01b2261050d99c30947d9a8
Minh <vanminh00052@gmail.com>
2026-04-19 12:52:57 +0700

    Add solution for Log Level -- error

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -2,16 +2,23 @@ static class LogLine
 {
     public static string Message(string logLine)
     {
-        
+        int idx = logLine.IndexOf(":");
+        string afterdot = logLine.Substring(idx + 2);
+        return afterdot.Trim();
     }
 
     public static string LogLevel(string logLine)
     {
+        int indx = logLine.IndexOf(":");
+        string afterhc = logLine.Substring(indx + 2);
+        int indx2 = logLine.IndexOf("/r");
+        string space = logLine.Substring(indx2 + 1);
+        return space.Trim().ToLower();
     }
 
     public static string Reformat(string logLine)
     {
 
     }
-    
+
 }
