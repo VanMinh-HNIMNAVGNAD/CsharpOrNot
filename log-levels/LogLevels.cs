5f25f804fa2f484e6d26f4fa5e3e32e44fd6ed01
Minh <vanminh00052@gmail.com>
2026-04-26 21:57:41 +0700

    Add solution for Log Level -- fix bug

--- log-levels/LogLevels.cs
+++ log-levels/LogLevels.cs
@@ -2,22 +2,22 @@ static class LogLine
 {
     public static string Message(string logLine)
     {
-        int idx = logLine.IndexOf(":");
-        string afterdot = logLine.Substring(idx + 2);
-        return afterdot.Trim();
+        int idx = logLine.IndexOf(":"); // lấy vị trí kí tự 
+        string afterdot = logLine.Substring(idx + 2); // cắt từ kí tự đó và khoảng trắng
+        return afterdot.Trim(); // cắt khoảng trắng 2 đầu 
     }
 
     public static string LogLevel(string logLine)
     {
-        int st = logLine.IndexOf('[') + 1;
-        int ed = logLine.IndexOf(']');
-        return logLine.Substring(st, ed - st).ToLower();
+        int st = logLine.IndexOf('[') + 1; // lấy vị trí bắt đầu từ vị trí + 1 trong chuỗi
+        int ed = logLine.IndexOf(']'); // lấy vị trí cuối cùng trong chuỗi cần lấy 
+        return logLine.Substring(st, ed - st).ToLower(); // bắt đầu từ +1, lấy cuối trừ đầu ra số lượng text cần in ra, viết thường 
 
     }
 
     public static string Reformat(string logLine)
     {
-        return $"{Message(LogLine)} ({LogLevel(logLine)})";
+        return $"{Message(logLine)} ({LogLevel(logLine)})"; 
     }
 
 }
