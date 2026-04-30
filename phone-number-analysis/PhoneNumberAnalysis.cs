789d240c047ffbe6a33c9373d0dcc15e7d36e3d8
Minh <vanminh00052@gmail.com>
2026-04-30 21:03:42 +0700

    Add solution for phone-number-analysis

--- phone-number-analysis/PhoneNumberAnalysis.cs
+++ phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,11 +2,18 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        string areaCode = phoneNumber.Substring(0, 3);
+        string pre = phoneNumber.Substring(4, 3);
+        string localNum = phoneNumber.Substring(8, 4);
+
+        bool isNY = areaCode == "212";
+        bool isFake = pre == "555";
+
+        return (isNY, isFake, localNum);
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+        return phoneNumberInfo.IsFake;
     }
 }
