96cc17b47b7522e81f0d702ecd049f417664e5bd
Minh <vanminh00052@gmail.com>
2026-04-30 15:45:14 +0700

    Add solution for Log Analysis -- error

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -3,14 +3,20 @@ public static class LogAnalysis
     // TODO: define the 'SubstringAfter()' extension method on the `string` type
     public static string SubstringAfter(this string str, string delimiter)
     {
-        int idx = str.IndexOf(delimiter);
-        int start = idx + delimiter.Length;
-        return start;
+        int idx = str.IndexOf(delimiter[": "]);
+        int start = idx + delimiter.Length + 2;
+        string end = str[start, delimiter - start];
+        return end;
     }
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    public static string SubstringBetween(this string str )
+    public static string SubstringBetween(this string str, string stdelimiter, string eddelimiter )
     {
+        int leftidx = stdelimiter.IndexOf(str["["]);
+        int contentidx = leftidx + stdelimiter.Length;
+        int rightidx = eddelimiter.IndexOf(contentidx["]"]);
+        int leth = rightidx - contentidx;
         
+         
     }
     // TODO: define the 'Message()' extension method on the `string` type
     public static string Message(this string str)
