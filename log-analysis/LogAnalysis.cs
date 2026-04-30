dd8dfb6da0263f8f7dc852498205c49cf0fca3f3
Minh <vanminh00052@gmail.com>
2026-04-30 14:55:45 +0700

    Add solution for Log Analysis

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -1,10 +1,25 @@
 public static class LogAnalysis 
 {
     // TODO: define the 'SubstringAfter()' extension method on the `string` type
-
+    public static string SubstringAfter(this string str, string delimiter)
+    {
+        int idx = str.IndexOf(delimiter);
+        int start = idx + delimiter.Length;
+        return start;
+    }
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
+    public static string SubstringBetween(this string str )
+    {
+        
+    }
     // TODO: define the 'Message()' extension method on the `string` type
-
+    public static string Message(this string str)
+    {
+        
+    }
     // TODO: define the 'LogLevel()' extension method on the `string` type
+    public static string LogLevel(this string str)
+    {
+        
+    }
 }
