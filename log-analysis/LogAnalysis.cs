7e2766eb50801279da92d2235b83f9fd0fa0d2f1
Minh <vanminh00052@gmail.com>
2026-04-30 16:49:50 +0700

    Add solution for Log Analysis

--- log-analysis/LogAnalysis.cs
+++ log-analysis/LogAnalysis.cs
@@ -3,31 +3,31 @@ public static class LogAnalysis
     // TODO: define the 'SubstringAfter()' extension method on the `string` type
     public static string SubstringAfter(this string str, string delimiter)
     {
-        int idx = str.IndexOf(delimiter[": "]);
-        int start = idx + delimiter.Length + 2;
-        string end = str[start, delimiter - start];
+        int idx = str.IndexOf(delimiter);
+        int start = idx + delimiter.Length;
+        string end = str.Substring(start);
         return end;
     }
     // TODO: define the 'SubstringBetween()' extension method on the `string` type
     public static string SubstringBetween(this string str, string stdelimiter, string eddelimiter )
     {
-        int leftidx = stdelimiter.IndexOf(str["["]);
+        int leftidx = str.IndexOf(stdelimiter);
         int contentidx = leftidx + stdelimiter.Length;
-        int rightidx = eddelimiter.IndexOf(contentidx["]"]);
+        int rightidx = str.IndexOf(eddelimiter, contentidx);
         int leth = rightidx - contentidx;
-        int result = str[contentidx, leth];
+        string result = str.Substring(contentidx, leth);
         return result;
     }
     // TODO: define the 'Message()' extension method on the `string` type
     public static string Message(this string str)
     {
-        int message = str.SubstringAfter(": ");
+        string message = str.SubstringAfter(": ");
         return message;
     }
     // TODO: define the 'LogLevel()' extension method on the `string` type
     public static string LogLevel(this string str)
     {
-        int loglevel = SubstringBetween(str, "[", "]");
-        return loglevel.ToLower();
+        string loglevel = SubstringBetween(str, "[", "]");
+        return loglevel;
     }
 }
